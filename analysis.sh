a4167b6bc6a6f2a7b6dee618c85d252b4bdc8857
aginni <36461685+aginnimb@users.noreply.github.com>
2020-07-09 13:21:40 -0400

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-python3 JS_test-Copy1.py
-python3 Cluster_test-Copy1.py
+# To call the two python scripts Jaccard_index and Cluster_analysis and implements the algorithms with the python scripts.
+
+#Contains Jaccard Index algorithm that reads, cleanse and scores the chemical compounds and genes and writes the output to a csv file
+python3 Jaccard_Index.py
+
+#Clustering analysis algorithm that reads the score files, data merge,matrix conversion and hierarchical clustering.
+python3 Cluster_analysis.py
 
